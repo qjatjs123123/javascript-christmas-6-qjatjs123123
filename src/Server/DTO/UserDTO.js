e816a0a49de1ffd6bd3ee59063d9aea07827a6fd
Hong Boem Sun <qjatjs123123@naver.com>
2023-11-10 19:09:39 +0900

    refactor: UserDTO Validation 로직 별도 모듈로 분리

--- src/Server/DTO/UserDTO.js
+++ src/Server/DTO/UserDTO.js
@@ -1,5 +1,5 @@
-import { ERROR_MESSAGE } from '../../Util/Message.js';
 import { CONSTANTS, MENU, MENUFUNC } from '../../Util/Constants.js';
+import Validation from './Validation.js';
 
 class UserDTO {
   #expectedVisitDate;
@@ -35,7 +35,7 @@ class UserDTO {
   }
 
   setExpectedVisitDate(expectedVisitedDate) {
-    this.#checkDateInvalid(expectedVisitedDate);
+    Validation.checkDateInvalid(expectedVisitedDate);
     this.#expectedVisitDate = Number(expectedVisitedDate);
   }
 
@@ -53,51 +53,13 @@ class UserDTO {
     });
   }
 
-  #checkDateInvalid(expectedVisitedDate) {
-    if (expectedVisitedDate === '') throw new Error(ERROR_MESSAGE.isBlank);
-
-    const date = Number(expectedVisitedDate);
-    if (Number.isNaN(date)) throw new Error(ERROR_MESSAGE.isChar);
-    if (date < CONSTANTS.eventStartDate || date > CONSTANTS.eventEndDate)
-      throw new Error(ERROR_MESSAGE.isNotDateRange);
-  }
-
   #checkOrderMenuInvalid(orderMenus) {
     orderMenus.forEach((orderMenu) => {
-      this.#checkOrderMenuIsFormat(orderMenu);
-      this.#checkOrderMenuIsExist(orderMenu);
+      Validation.checkOrderMenuIsFormat(orderMenu);
+      Validation.checkOrderMenuIsExist(orderMenu);
     });
-    this.#checkOrderMenuIsDuplicate(orderMenus);
-    this.#checkOrderMenuIsAllBeverage(orderMenus);
-  }
-
-  #checkOrderMenuIsFormat(orderMenu) {
-    const regex = /.+-[1-9]\d*/;
-    if (!regex.test(orderMenu)) throw new Error(ERROR_MESSAGE.isNotOrderMenuFormat);
-  }
-
-  #checkOrderMenuIsExist(orderMenu) {
-    const menuName = orderMenu.split(CONSTANTS.countSplitChar)[0];
-    const category = MENUFUNC.getCategory(menuName);
-    if (category === false) throw new Error(ERROR_MESSAGE.isNotOrderMenuFormat);
-  }
-
-  #checkOrderMenuIsDuplicate(orderMenus) {
-    const orderMenuNameList = orderMenus.map(
-      (orderMenu) => orderMenu.split(CONSTANTS.countSplitChar)[0],
-    );
-    const orderMenuNameSet = new Set(orderMenuNameList);
-
-    if (orderMenuNameList.length !== orderMenuNameSet.size)
-      throw new Error(ERROR_MESSAGE.isDuplicate);
-  }
-
-  #checkOrderMenuIsAllBeverage(orderMenus) {
-    const orderMenuNameList = orderMenus
-      .map((orderMenu) => orderMenu.split(CONSTANTS.countSplitChar)[0])
-      .every((menuName) => MENU[CONSTANTS.foodOnlyNotAllow].has(menuName));
-
-    if (orderMenuNameList) throw new Error(ERROR_MESSAGE.isAllBeverage);
+    Validation.checkOrderMenuIsDuplicate(orderMenus);
+    Validation.checkOrderMenuIsAllBeverage(orderMenus);
   }
 
   get orderMenu() {
