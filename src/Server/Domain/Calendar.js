e38f5ba8a1375fd3b5e213aff8b4c08d40edec00
Hong Boem Sun <qjatjs123123@naver.com>
2023-11-11 21:50:20 +0900

    refactor: Calendar클래스 공통메서드(이벤트기간체크)를 분리 리팩토링

--- src/Server/Domain/Calendar.js
+++ src/Server/Domain/Calendar.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { CONSTANTS } from '../../Util/Constants.js';
 
 class Calendar {
@@ -11,37 +12,37 @@ class Calendar {
   }
 
   isChristmasDdayEvent(visitDay) {
-    const visitDate = this.#getDate(visitDay);
-    const christMasEventStartDay = this.#getDate(CONSTANTS.christMasEventStartDay);
-    const christMasEventEndDay = this.#getDate(CONSTANTS.christMasEventEndDay);
-
-    if (christMasEventStartDay <= visitDate && christMasEventEndDay >= visitDate) return true;
-    return false;
+    return this.#checkChristmasDdayEvent(visitDay);
   }
 
   isWeekDayEvent(visitDay) {
     const visitDate = this.#getDate(visitDay);
-    const eventStartDay = this.#getDate(CONSTANTS.eventStartDay);
-    const eventEndDay = this.#getDate(CONSTANTS.eventEndDay);
-
-    if (eventStartDay > visitDate && eventEndDay < visitDate) return false;
-    if (!CONSTANTS.weekdayDiscount.includes(visitDate.getDay())) return false;
-    return true;
+    return this.#checkEventPeriod(visitDay) && CONSTANTS.weekdayDiscount.includes(visitDate.getDay());
   }
 
   isWeekEndEvent(visitDay) {
     const visitDate = this.#getDate(visitDay);
-    const eventStartDay = this.#getDate(CONSTANTS.eventStartDay);
-    const eventEndDay = this.#getDate(CONSTANTS.eventEndDay);
-
-    if (eventStartDay > visitDate && eventEndDay < visitDate) return false;
-    if (!CONSTANTS.weekendDiscount.includes(visitDate.getDay())) return false;
-    return true;
+    return this.#checkEventPeriod(visitDay) && CONSTANTS.weekendDiscount.includes(visitDate.getDay());
   }
 
   isSpecialEvent(visitDay) {
-    if (CONSTANTS.specialDiscount.includes(visitDay)) return true;
-    return false;
+    return this.#checkEventPeriod(visitDay) && CONSTANTS.specialDiscount.includes(visitDay);
+  }
+
+  #checkChristmasDdayEvent(visitDay) {
+    const visitDate = this.#getDate(visitDay);
+    const christMasEventStartDay = this.#getDate(CONSTANTS.christMasEventStartDay);
+    const christMasEventEndDay = this.#getDate(CONSTANTS.christMasEventEndDay);
+
+    return christMasEventStartDay <= visitDate && christMasEventEndDay >= visitDate;
+  }
+
+  #checkEventPeriod(visitDay) {
+    const visitDate = this.#getDate(visitDay);
+    const eventStartDay = this.#getDate(CONSTANTS.eventStartDay);
+    const eventEndDay = this.#getDate(CONSTANTS.eventEndDay);
+
+    return eventStartDay <= visitDate && eventEndDay >= visitDate;
   }
 
   #getDate(day) {
