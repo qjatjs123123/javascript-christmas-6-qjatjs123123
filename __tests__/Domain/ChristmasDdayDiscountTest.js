52397902d86c45224d9964ea15e8eb0d40f21558
Hong Boem Sun <qjatjs123123@naver.com>
2023-11-14 14:15:43 +0900

    test: 크리스마스 디데이 25일 이후 할인 이벤트 적용 안되는지 확인 테스트코드 작성

--- __tests__/Domain/ChristmasDdayDiscountTest.js
+++ __tests__/Domain/ChristmasDdayDiscountTest.js
@@ -1,15 +1,19 @@
 /* eslint-disable prettier/prettier */
 import ChristmasDdayDiscount from '../../src/Server/Domain/ChristmasDdayDiscount';
 import UserDTO from '../../src/Server/DTO/UserDTO.js';
+import EventController from '../../src/Server/Controller/EventController.js';
+import { CONSTANTS } from '../../src/Util/Constants.js';
 
 describe('크리스마스 디데이 할인 테스트', () => {
   let christmas;
   let userDTO;
+  let eventController;
   const inputOrderMenu = '티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1';
 
   beforeEach(() => {
     userDTO = new UserDTO();
     christmas = new ChristmasDdayDiscount();
+    eventController = new EventController();
   });
 
   describe('크리스마스 디데이는 1,000원으로 시작하여 크리스마스가 다가올수록 날마다 할인 금액이 100원씩 증가한다.', () => {
@@ -24,10 +28,34 @@ describe('크리스마스 디데이 할인 테스트', () => {
       // when
       userDTO.setExpectedVisitDate(input);
       userDTO.setOrderMenu(inputOrderMenu);
-      christmas.discount(userDTO);
+      eventController.handleDiscountEvent(userDTO);
+      const discountHistory = userDTO.getDisCountHistory();
+      const result = discountHistory.filter(({ eventName }) => eventName === CONSTANTS.christmasDdayEventName);
 
       // then
-      expect(expected).toEqual(userDTO.getDisCountHistory()[0].discount);
+      expect(expected).toEqual(result[0].discount);
     });
   });
+
+  describe('크리스마스 디데이는 25일이 지나면 이벤트가 종료된다.', () => {
+    const testCases = [
+      { input: 26 },
+      { input: 27 },
+      { input: 31 },
+    ];
+
+    test.each(testCases)('getDisCountHistory에서 할인 내역에 크리스마스 디데이가 없어야 한다.', ({ input }) => {
+      // when
+      userDTO = new UserDTO();
+      userDTO.setExpectedVisitDate(input);
+      userDTO.setOrderMenu(inputOrderMenu);
+      eventController.handleDiscountEvent(userDTO);
+      const discountHistory = userDTO.getDisCountHistory();
+      const result = discountHistory.some(({ eventName }) => eventName === CONSTANTS.christmasDdayEventName);
+
+      // then
+      expect(result).toBe(false);
+    });
+  });
+
 });
